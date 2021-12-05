0b13ea4c36486ba28548fb4b8a90e880d1a096bb
bramaya <bramaya@aol.com>
2021-12-05 14:41:26 -0600

    js-l12-practise-exercise-1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const randomFolks = document.querySelector(".random-peeps");
 
 const getData = async function(){
     const usersRequest = await fetch("https//randomuser.me/api?results=5");
-    const data = await res.json();
+    const data = await usersRequest.json();
 
     const userResults = data.results; // array of objects
     displayUsers(userResults);
